81130ba4d5eb79df5add8b11deb430b07cab7920
dhahiba <dhhiba1991@gmail.com>
2023-06-26 02:56:07 -0700

    test

--- function.c
+++ function.c
@@ -3,28 +3,33 @@
  * printDigit - ..
  * @n: ...
  */
- void printDigit(int n) {
-        if (n == 0) {
-            return;
-        }
-        printDigit(n / 10);
-        putchar('0' + (n % 10));  // Convert the digit to character representation
-    }
+void printDigit(int n)
+{
+if (n == 0)
+{
+return;
+}
+printDigit(n / 10);
+putchar('0' + (n % 10));
+}
 /**
  * printInteger - ...
  * @num: ...
  */
 
-void printInteger(int num) {
-    if (num < 0) {
-        putchar('-');  // Print a minus sign for negative numbers
-        num = -num;    // Convert the number to positive
-    }
+void printInteger(int num)
+{
+if (num < 0)
+{
+putchar('-');
+num = -num;
+}
 
-    if (num == 0) {
-        putchar('0');  // Print '0' for zero
-        return;
-    }
+if (num == 0)
+{
+putchar('0');
+return;
+}
 
-    printDigit(num);
+printDigit(num);
 }
